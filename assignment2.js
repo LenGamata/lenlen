ec7334b7e30a2dca1cfd98ecb30e66ee64f5ab8d
LenGamata <lengamata14@gmail.com>
2024-09-24 07:00:45 +0700

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,26 @@
+var a = 25;
+var b = 20;
+var c = 12;
+var d = "15";
+var e = 15;
+
+var sumTotal = a + b + c + (d) + e;
+console.log("Sum of a, b. c, d, and e,", sumTotal);
+
+var dInt = preseInt(d);  //
+console.log("d > e");
+console.log("d < e");
+console.log("d >= e");
+console.log("d <= e");
+console.log("d == e");
+
+var subtractionResult = a - b - c - dInt - e;
+console.log("Result of Subtraction");
+
+var multipicationDivisionResult = (a * c) / e;
+console.log("Result of (a * c) / e:");
+
+var exponentResult = Math.pow(e,c);
+console.log("Exponent value of e raised to c:");
+
+console.log("Reassinged value of e (exponent result):");
---
+++ assingment1.js
@@ -0,0 +1,33 @@
+// Persona 1:
+var firstName1 = "Rex Bryan";
+var middleName1 = "Gabayan";
+var lastName1 = "Hufemia";
+var birthdate1 = "October 10, 2003";
+var birthplace1 = "Resonable Candon city, Ilocos Sur";
+var address1 = "Butol, Santiago, Ilocos Sur";
+var course1 = "Bachelor of Science in Political Science";
+var dreamjob1 = "Policy Analys";
+
+// Pesona 2:
+var firstName2 = "Rose Anne";
+var middleName2 = "Dollente";
+var latName2 = "Chico";
+var birthdate2 = "September 22, 2005";
+var birthplace2 = "City Hospital , Candon City Ilocos Sur";
+var adress2 = "Ambucao, Santiago, Ilocos Sur";
+var course2 = "Bachelor of Science in Compute Science";
+var dreamjob2 = "Softwer Engineer";
+
+// string
+var output1 = [firstName1.toUpperCase() + middleName1.toUpperCase() + lastName1.toUpperCase()] + "was born";
+var output1 = [birthdate1.toUpperCase() + birthplace1.toUpperCase()] + "and currently  living at" + [adress1.toUpperCase()];
+var output1 = [firsName1.toLowerCase() + middleName1.toLowerCase() + lastName1.toLowerCase()] + "is taking up";
+var output1 = [course1.toLowerCaase()] + "and Dreams to be"; + [dreamJob1] + "after Graduation";
+
+var output2 = [firstName2.toUpperCase() + middleName2.toUpperCas() + lastName1.toUpperCase()] + "was born";
+var output2 = [birthdate2.toUpperCase() + birthplace2.toUpperCase()] + "and currently living" + [adress2.toUpperCase()];
+var output2 = [firsName2.toLowerCase() + middleName2.toLowerCase() + lastName2.toLowerCase()] + "is taking up";
+var output2 = [course2.toLowerCaase()] + "and Dreams to be"; + [dreamJob2] + "after Graduation";
+
+console.log(output1);
+console.log(output2);
